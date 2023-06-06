b7dbb4b7df86864c050135e23f0e3a0fb915616b
eSarmir <e.sarmir@gmail.com>
2023-06-06 22:06:38 +0200

    Adds Instruction entity with configuration

---
+++ Savorscape.Database/Models/Instruction.cs
@@ -0,0 +1,10 @@
+﻿namespace Savorscape.Database.Models
+{
+    public class Instruction
+    {
+        public int InstructionID { get; set; }
+        public int Order { get; set; }
+        public required string Description { get; set; }
+        public required Recipe Recipe { get; set; }
+    }
+}
