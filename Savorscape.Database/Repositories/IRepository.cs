4ee467393c36e385d4d6dce8672992e295371da7
eSarmir <e.sarmir@gmail.com>
2023-06-07 18:01:43 +0200

    Removes unnecessary methods from generic repository

--- Savorscape.Database/Repositories/IRepository.cs
+++ Savorscape.Database/Repositories/IRepository.cs
@@ -4,11 +4,6 @@ namespace Savorscape.Database.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        IEnumerable<TEntity> Get(
-            Expression<Func<TEntity, bool>> filter,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy,
-            string includeProperties = "");
-        IEnumerable<TEntity> GetAll();
         TEntity? GetByID(int id);
         void Create(TEntity entity);
         void Update(TEntity entity);
