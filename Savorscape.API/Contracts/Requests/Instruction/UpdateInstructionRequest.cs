7e9058931432e1b9fdb93d23b9ac639e2b104cbd
eSarmir <e.sarmir@gmail.com>
2023-06-10 22:12:26 +0200

    Adds validation attributes to request contracts, adds MustBePositiveInteger validation attribute

--- Savorscape.API/Contracts/Requests/Instruction/UpdateInstructionRequest.cs
+++ Savorscape.API/Contracts/Requests/Instruction/UpdateInstructionRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace Savorscape.API.Contracts.Requests.Instruction
+﻿using Savorscape.API.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Savorscape.API.Contracts.Requests.Instruction
 {
-    public record UpdateInstructionRequest(
-        int InstructionID,
-        int Order,
-        string Description,
-        int RecipeID
-        );
+    public record UpdateInstructionRequest
+    {
+        [Required]
+        public int InstructionID { get; init; }
+        [Required]
+        [MustBePositiveInteger]
+        public int Order { get; init; }
+        [Required]
+        [MaxLength(InstructionConstraints.DescriptionMaxLength)]
+        public required string Description { get; init; }
+        [Required]
+        public int RecipeID { get; init; }
+    }
 }
