c54b95431a1e555b09b2a946c90d68d88619d04a
eSarmir <e.sarmir@gmail.com>
2023-06-10 12:33:29 +0200

    Renames InstructionsResponse

--- Savorscape.API/Contracts/Responses/Instructions/InstructionsResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Savorscape.API.Contracts.Responses.Instructions
-{
-    public record InstructionsResponse(
-        int InstructionID,
-        int Order,
-        string Description,
-        int RecipeId
-        );
-}
