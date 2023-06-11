64bad0753660faa09a9f7d6526407034560b247a
eSarmir <e.sarmir@gmail.com>
2023-06-11 20:25:07 +0200

    Adds fluent result, implements match extension

---
+++ Savorscape.API/Extensions/ResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Savorscape.API.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static IActionResult MatchToActionResult<A>(this Result<A> result, Func<A, IActionResult> Succ, Func<IError, IActionResult> Fail) =>
+           result.IsFailed
+               ? Fail(result.Errors.First())
+               : Succ(result.Value);
+    }
+}
